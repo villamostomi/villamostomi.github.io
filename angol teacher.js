d8c042efb76261f2df0450749e421c9d0946bfed
villamostomi <66127322+villamostomi@users.noreply.github.com>
2020-09-15 16:39:20 +0100

    Add files via upload

---
+++ angol teacher.js
@@ -0,0 +1,111 @@
+
+
+
+   function click1() {
+     
+      setTimeout(keyTimeout1, 100)
+      setTimeout(keyTimeout2, 2000)
+      setTimeout(keyTimeout3, 4000)
+      setTimeout(keyTimeout4, 6000)
+      setTimeout(keyTimeout5, 8000)
+      setTimeout(keyTimeout6, 10000)
+      setTimeout(keyTimeout7, 12000)
+      setTimeout(keyTimeout8, 14000)
+      setTimeout(keyTimeout9, 16000)
+      
+
+      
+        
+
+      }
+ 
+   function keyTimeout1() {
+    
+      document.getElementById("elsosor").innerHTML = "CARELESS";
+      document.getElementById("masodiksor").innerHTML = "gondatlan, figyelmetlen";
+     document.getElementById("window").style.color = "orange";
+     document.getElementById("window").style.background = "blue";
+
+      var x = document.createElement("AUDIO");
+     x.setAttribute("src","audio/careless.mp3");
+     x.play();  
+     
+     }
+     function keyTimeout2() {
+        var x = document.createElement("AUDIO");
+        x.setAttribute("src","audio/careless.mp3");
+        x.play();  
+      }
+   function keyTimeout3() {
+    
+      document.getElementById("elsosor").innerHTML = "He is a careless driver";
+      document.getElementById("masodiksor").innerHTML = "Ő egy figyelmetlen sofőr";
+     document.getElementById("window").style.color = "orange";
+     document.getElementById("window").style.background = "blue";
+
+      var x = document.createElement("AUDIO");
+     x.setAttribute("src","audio/careless exp.mp3");
+     x.play();  
+     
+     }
+    
+   function keyTimeout4() {
+    
+      document.getElementById("elsosor").innerHTML = "SUBSCRIBE";
+      document.getElementById("masodiksor").innerHTML = "feliratkozni";
+     document.getElementById("window").style.color = "orange";
+     document.getElementById("window").style.background = "blue";
+
+      var x = document.createElement("AUDIO");
+     x.setAttribute("src","audio/subscribe.mp3");
+     x.play();  
+     
+     }
+     function keyTimeout5() {
+        var x = document.createElement("AUDIO");
+        x.setAttribute("src","audio/subscribe.mp3");
+        x.play();  
+      }
+   function keyTimeout6() {
+    
+      document.getElementById("elsosor").innerHTML = "We subscribed to a newspaper";
+      document.getElementById("masodiksor").innerHTML = "Feliratkoztunk egy újságra";
+     document.getElementById("window").style.color = "orange";
+     document.getElementById("window").style.background = "blue";
+
+      var x = document.createElement("AUDIO");
+     x.setAttribute("src","audio/subscribe exp.mp3");
+     x.play();  
+     
+     }
+   function keyTimeout7() {
+    
+      document.getElementById("elsosor").innerHTML = "USELESS";
+      document.getElementById("masodiksor").innerHTML = "haszontalan, hiábavaló";
+     document.getElementById("window").style.color = "orange";
+     document.getElementById("window").style.background = "blue";
+
+      var x = document.createElement("AUDIO");
+     x.setAttribute("src","audio/useless.mp3");
+     x.play();  
+     
+     }
+     function keyTimeout8() {
+        var x = document.createElement("AUDIO");
+        x.setAttribute("src","audio/useless.mp3");
+        x.play();  
+      }
+   function keyTimeout9() {
+    
+      document.getElementById("elsosor").innerHTML = "This is a useless tool.";
+      document.getElementById("masodiksor").innerHTML = "Ez egy haszontalan eszköz.";
+     document.getElementById("window").style.color = "orange";
+     document.getElementById("window").style.background = "blue";
+
+      var x = document.createElement("AUDIO");
+     x.setAttribute("src","audio/useless exp.mp3");
+     x.play();  
+     
+     }
+    
+     
